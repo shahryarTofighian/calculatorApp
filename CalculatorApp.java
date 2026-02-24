7bee59aa8754484f4b4c0b990d496592427d549d
Tefo <104619848+shahryarTofighian@users.noreply.github.com>
2026-02-24 01:40:38 +0100

    changed format

--- CalculatorApp.java
+++ CalculatorApp.java
@@ -1,11 +1,16 @@
 package org.example;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalculatorApp {
 
     public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+
         Scanner sc = new Scanner(System.in);
 
         CalculatorService calc = new CalculatorService();
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # calculatorApp
 This app is about calculator application built in 2 stage (1. simple math 2.complicated math)
+hello
