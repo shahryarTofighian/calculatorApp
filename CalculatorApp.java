ff3b9c42ea72e56f6074062ec5f8c2077adaa7ea
Tefo <104619848+shahryarTofighian@users.noreply.github.com>
2026-02-24 01:46:39 +0100

    fix

--- CalculatorApp.java
+++ CalculatorApp.java
@@ -8,7 +8,7 @@ public class CalculatorApp {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("hello");
         CalculatorService calc = new CalculatorService();
         HistoryService history = new HistoryService();
 
